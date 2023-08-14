68d82c70d3883ae43b46e5947a3ff29c99d62239
Ben Jefferson <benj@benj-laptop.local>
2023-08-14 16:35:51 +0100

    Added in font conversion tools for fpdf and Arimo font

--- scripts/fpdfFonts/makefont.php
+++ scripts/fpdfFonts/makefont.php
@@ -1,9 +1,50 @@
 <?php
 require('makefont/makefont.php');
 
-// Font files were sourced from here:
-https://github.com/matomo-org/travis-scripts/tree/master/fonts
+$fontLibDir = implode( DIRECTORY_SEPARATOR,[__DIR__,'..','..','lib','fpdfFonts','']);
+if (!is_dir($fontLibDir)) {
+    if (mkdir($fontLibDir)) {
+        echo "Created directory for fpdf Fonts: $fontLibDir\n";
+    } else {
+        echo "Couldn't create directory for fpdf Fonts: $fontLibDir\n";
+        exit;
+    }
+}
 
+$fontDir = __DIR__.DIRECTORY_SEPARATOR.'ttfFonts';
+if (!is_dir($fontDir)) {
+    echo "Couldn't open font directory: $fontDir\n";
+    exit;
+}
 
-MakeFont(,'cp1252');
+$fontDirHandle = opendir($fontDir);
+$ttfFiles = [];
+while ($subDir = readdir($fontDirHandle)) {
+    $subDirPath = $fontDir.DIRECTORY_SEPARATOR.$subDir;
+    if (substr($subDir,0,1)=='.') continue;
+    if (!is_dir($subDirPath)) continue;
+    echo "Opening directory: $subDir\n";
+    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($subDirPath));
+
+    foreach ($files as $file) {
+        if ($file->isFile() && pathinfo($file->getFilename(), PATHINFO_EXTENSION) === 'ttf') {
+            // Ignore Variable Weight fonts
+            if (strpos($file->getFilename(),'VariableFont_wght')) continue;
+            $ttfFiles[] = $file->getRealPath();
+        }
+    }
+
+}
+echo "Found the following font files for processing:\n";
+print_r($ttfFiles);
+
+foreach( $ttfFiles as $ttfFile ) {
+    // See http://www.fpdf.org/en/tutorial/tuto7.htm
+    // The cp1252 here is a bit of a guess!
+    MakeFont($ttfFile,'cp1252',true);
+    $fontFile = basename($ttfFile);
+    $fontFile = preg_replace('/\\.ttf$/i','',$fontFile);
+    rename($fontFile.'.php',$fontLibDir.$fontFile.'.php');
+    rename($fontFile.'.z',$fontLibDir.$fontFile.'.z');
+}
 ?>
--- scripts/fpdfFonts/makefont/makefont.php
+++ scripts/fpdfFonts/makefont/makefont.php
@@ -422,26 +422,3 @@ function MakeFont($fontfile, $enc='cp1252', $embed=true, $subset=true)
 	MakeDefinitionFile($basename.'.php', $type, $enc, $embed, $subset, $map, $info);
 	Message('Font definition file generated: '.$basename.'.php');
 }
-
-if(PHP_SAPI=='cli')
-{
-	// Command-line interface
-	ini_set('log_errors', '0');
-	if($argc==1)
-		die("Usage: php makefont.php fontfile [encoding] [embed] [subset]\n");
-	$fontfile = $argv[1];
-	if($argc>=3)
-		$enc = $argv[2];
-	else
-		$enc = 'cp1252';
-	if($argc>=4)
-		$embed = ($argv[3]=='true' || $argv[3]=='1');
-	else
-		$embed = true;
-	if($argc>=5)
-		$subset = ($argv[4]=='true' || $argv[4]=='1');
-	else
-		$subset = true;
-	MakeFont($fontfile, $enc, $embed, $subset);
-}
-?>
