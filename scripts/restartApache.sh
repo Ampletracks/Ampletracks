71f982ba56d6c2472f485b85c17977dd54af97ca
Ben Jefferson <ben@whattogive.com>
2023-11-22 12:17:56 +0000

    Added in support for other Linux variants

--- scripts/restartApache.sh
+++ scripts/restartApache.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 certificate=../data/acme/certificate.crt
-pidFile=/var/run/apache2/apache2.pid
+
+# Check if apache2 is installed
+if command -v apache2 >/dev/null 2>&1; then
+    service=apache2
+# Check if httpd is installed
+elif command -v httpd >/dev/null 2>&1; then
+    service=httpd
+else
+    echo "Neither apache2 nor httpd is installed"
+    exit;
+fi
+
+pidFile=/var/run/$service/$service.pid
 
 # Check if the SSL certificate for this site is newer than the current Apache process
 # If it is then restart apache
@@ -10,5 +22,5 @@ if [ ! -e "$certificate" ]; then exit; fi
 if [ ! -e "$pidFile" ]; then exit; fi
 
 if [ "$certificate" -nt "$pidFile" ]; then
-	/usr/sbin/service apache2 restart
+	/usr/sbin/service $service restart
 fi
