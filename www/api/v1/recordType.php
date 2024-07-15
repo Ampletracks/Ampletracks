00efb7c410c9995ac1f4fcd860bbd349f3f8ce44
Ben Jefferson <benj@benj-laptop.local>
2024-07-15 10:54:20 +0100

    Various modifications to API code and adding recordType

---
+++ www/api/v1/recordType.php
@@ -0,0 +1,32 @@
+<?
+# define('API_ITEMS_PER_PAGE', 2);
+
+$API_SQL = [
+    'idList' => '
+        SELECT DISTINCT recordType.id
+        FROM recordType
+        WHERE
+            recordType.deletedAt=0
+    ',
+    'getData' => '
+        SELECT
+            recordType.id,
+            recordType.name,
+            recordType.colour,
+            CONCAT("rt_", recordType.apiId) AS apiId,
+            dataField.id AS primaryDataFieldId,
+            CONCAT("df_", dataField.apiId) AS primaryDataFieldApiId
+        FROM
+            recordType
+            LEFT JOIN dataField ON dataField.id=recordType.primaryDataFieldId AND dataField.recordTypeId=recordType.id AND !dataField.deletedAt
+        WHERE recordType.deletedAt=0 AND recordType.id IN (?)
+    '
+];
+
+$API_ID_MAPPINGS = [
+    'recordType' => [ 'id', 'apiId' ],
+    'dataField' => [ 'primaryDataFieldId', 'primaryDataFieldApiId' ]
+];
+
+require('../../../lib/api/startup.php');
+
